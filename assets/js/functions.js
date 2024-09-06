1820a3b73872aa5caeea171a697ea1d5d096a537
Nguyen Minh Hieu <lotous12a4@gmail.com>
2024-09-06 11:43:06 +0700

    Add a function to compress a string by grouping consecutive characters

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,22 @@
-export function greeting() {
-    alert("Hello");
+// Tổng hợp bài tập duyệt mảng #2
+
+/**
+ * Viết hàm compressString để rút gọn một chuỗi bằng cách thay thế các nhóm ký tự liên tiếp giống nhau bằng một ký tự đó kèm theo số lần xuất hiện liên tiếp của ký tự đó.
+*/
+
+export function compressString(str) {
+    let compress = '';
+    let count = 1;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            compress += str[i] + count;
+            count = 1;
+        }
+    }
+    return compress;
 }
+
+// console.log(compressString("aaabbc")); // a3b2c1
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
-import { greeting } from "./functions.js";
+import { compressString } from "./functions.js";
 
-greeting()
+// Sample usage
+console.log(compressString("aaabbc")); // a3b2c1
+console.log(compressString("aabcccccaaa")); // a2b1c5a3
